13c0d3c00a0101209cb5dd01bae8802a778a4bc7
Gabriella Marusyak <slipuhinag@gmail.com>
2023-11-23 13:13:19 +0200

    Finished HW-03

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+    private signature: number = Math.random()
+  
+    getSignature() : number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key){
+        this.key = key;
+    }
+    getKey(): Key{
+        return this.key;
+    }
+}
+abstract class House {
+    private tenants: Person[] = [];
+    public door: boolean = false;
+    constructor ( protected  key: Key){
+        this.key = key;
+    }
+
+   public comeIn(person: Person): void {
+        if (this.door) {
+          this.tenants.push(person);
+     }
+}
+   public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+          }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
